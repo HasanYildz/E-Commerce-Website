6bbfaad2f3fcf11baa17f9dd97e4607a7895ffac
HasanYildz <201118053@samsun.edu.tr>
2023-10-05 22:20:45 +0300

    1.05

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js";
-import { hashPassword } from "../helpers/authHelpers.js";
-
+import { comparePassword, hashPassword } from "../helpers/authHelpers.js";
+import JWT from "jsonwebtoken";
 
 export const registerController = async (req, res) => {
     try {
@@ -56,6 +56,67 @@ export const registerController = async (req, res) => {
             error
         })
 
+    }
+};
+
+// Post Login
+export const loginController = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        //validation
+        if (!email || !password) {
+            return res.status(404).send({
+                success: false,
+                message: "Invalid email or password",
+            });
+        }
+        //check user
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: "Email is not registered",
+            });
+        }
+        const match = await comparePassword(password, user.password);
+        if (!match) {
+            return res.status(200).send({
+                success: false,
+                message: "Invalid Password",
+            });
+        }
+        //token
+        const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
+        res.status(200).send({
+            success: true,
+            message: "login successfully",
+            user: {
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                address: user.address,
+            },
+            token,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in login",
+            error
+        });
+    }
+};
+
+//test controller
+export const testController = (req, res) => {
+    try {
+        res.send("Protected Routes");
+    } catch (error) {
+        console.log(error);
+        res.send({ error });
     }
 };
 
